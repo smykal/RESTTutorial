5ee91baa84b3e1e14f3223f57f77a43e70f438c4
Mateusz <smykal@engineer.com>
2022-01-03 16:52:51 +0100

    add spring-jpa, spring-web, h2, lombok

---
+++ src/main/java/com/RESTTutorial/RestTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.RESTTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTutorialApplication.class, args);
+	}
+
+}
